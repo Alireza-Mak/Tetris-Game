966a73685c12fcc64dde36b47f7ee81340a24ff9
Alireza Mak <info@alirezamak.com>
2025-05-17 20:55:32 -0700

    // Basic SFML test to verify that the SFML library is functioning correctly on an x86 system. // Creates a window and renders a green circle to confirm graphics rendering works as expected.

--- Tetris-Game/Tetris-Game.cpp
+++ Tetris-Game/Tetris-Game.cpp
@@ -2,7 +2,7 @@
 #include "Gameboard.h"
 #include "TestSuite1.h"
 #include "TestSuite2.h"
-
+#include <SFML/Graphics.hpp>
 
 
 int main()
@@ -12,6 +12,25 @@ int main()
 
 	// A test suite is a collection of tests that are run to verify the functionality of Gameboard Class.
 	TestSuite2::runTestSuite2();
+
+    // Simple SFML test to confirm graphics rendering works on x86 systems.
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
 
 	return  0;
 }
